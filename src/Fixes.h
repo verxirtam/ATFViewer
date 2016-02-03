893c2ad95ea3c4842364f35a0f7c06b0c81fb33b
verxirtam <d-kosuga@jcom.home.ne.jp>
2016-02-04 01:39:10 +0900

    Fixes.hを作成。描画用のオブジェクトを作成した。

---
+++ src/Fixes.h
@@ -0,0 +1,81 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Fixes.h
+ *
+ *    Description:  フィックスの描画を行う
+ *
+ *        Version:  1.0
+ *        Created:  2016年02月03日 23時52分27秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef FIXES_H_
+#define FIXES_H_
+
+#include <vector>
+#include <string>
+
+class Fixes
+{
+private:
+	//フィックス名
+	std::vector<std::string> name;
+	//経度
+	std::vector<double> longitude;
+	//緯度
+	std::vector<double> latitude;
+public:
+	Fixes()
+	{
+		name.push_back("haneda");
+		longitude.push_back(139.0+46.0/60.0+87.0/3600.0);
+		latitude.push_back(  35.0+33.0/60.0+20.0/3600.0);
+		name.push_back("AKARA");
+		longitude.push_back(123.0+29.0/60.0+54.0/3600.0);
+		latitude.push_back(  31.0+30.0/60.0+12.0/3600.0);
+		name.push_back("LAMEN");
+		longitude.push_back(123.0+59.0/60.0+54.0/3600.0);
+		latitude.push_back(  31.0+36.0/60.0+48.0/3600.0);
+		name.push_back("SADLI");
+		longitude.push_back(125.0+00.0/60.0+00.0/3600.0);
+		latitude.push_back(  31.0+49.0/60.0+48.0/3600.0);
+		name.push_back("NIRAT");
+		longitude.push_back(126.0+03.0/60.0+29.0/3600.0);
+		latitude.push_back(  32.0+03.0/60.0+54.0/3600.0);
+		name.push_back("ONIKU");
+		longitude.push_back(126.0+39.0/60.0+17.0/3600.0);
+		latitude.push_back(  32.0+11.0/60.0+42.0/3600.0);
+		name.push_back("POTET");
+		longitude.push_back(127.0+02.0/60.0+23.0/3600.0);
+		latitude.push_back(  32.0+16.0/60.0+48.0/3600.0);
+	}
+	void display()
+	{
+		for(unsigned int n = 0; n < longitude.size(); n++)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				double id = (double)i;
+				double d = 0.03125 * i;
+				glBegin(GL_LINE_LOOP);
+				glColor3d(1.0,1.0,0.0);
+				glVertex3d(longitude[n]-d, latitude[n]-d, 2000.0*id);
+				glVertex3d(longitude[n]+d, latitude[n]-d, 2000.0*id);
+				glVertex3d(longitude[n]+d, latitude[n]+d, 2000.0*id);
+				glVertex3d(longitude[n]-d, latitude[n]+d, 2000.0*id);
+				glEnd();
+			}
+		}
+	}
+};
+
+
+
+#endif
