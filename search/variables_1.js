9ca2903deabcf0f21076a053b245ab1d7bbafc2c
verxirtam <d-kosuga@jcom.home.ne.jp>
2016-08-17 11:41:34 +0900

    update docs.

--- search/variables_1.js
+++ search/variables_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['base',['base',['../classVAOPositionColorDynamic.html#a962ba0c4f21d59ddab66b4c1e6308fbe',1,'VAOPositionColorDynamic::base()'],['../classVBOStatic.html#ab7eea3ea2a902c5e40d3892a97a4c593',1,'VBOStatic::base()'],['../classVBOElementStatic.html#adc05bcdf5089d204c20f301b15dd1b13',1,'VBOElementStatic::base()'],['../classVBODynamicBase.html#ae54982a8773847f62c60f9741849e0f7',1,'VBODynamicBase::base()']]],
+  ['base',['base',['../classVAOPositionColorBase.html#ab968753104aad3fa00ad9508fbf076aa',1,'VAOPositionColorBase::base()'],['../classVAOPositionColorDynamic.html#a962ba0c4f21d59ddab66b4c1e6308fbe',1,'VAOPositionColorDynamic::base()'],['../classVAOPositionTexture.html#a01269b460634f565dfc067134b13f92e',1,'VAOPositionTexture::base()'],['../classVBOStatic.html#ab7eea3ea2a902c5e40d3892a97a4c593',1,'VBOStatic::base()'],['../classVBOElementStatic.html#adc05bcdf5089d204c20f301b15dd1b13',1,'VBOElementStatic::base()'],['../classVBODynamicBase.html#ae54982a8773847f62c60f9741849e0f7',1,'VBODynamicBase::base()']]],
   ['bufferpaths',['bufferPaths',['../classPaths.html#a9b97cb3a74ac2645f75d6ecaae136c69',1,'Paths']]],
   ['button',['Button',['../classJoystick.html#a96b69863a1ce0fb518ba3a61e01cfcdc',1,'Joystick']]],
   ['buttoncount',['ButtonCount',['../classJoystick.html#a8d77e735855a21ca8d6080cda62dd914',1,'Joystick']]]
--- search/variables_11.js
+++ search/variables_11.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['v',['v',['../structMap_1_1MapVertex.html#ae66e809e409d0651aab70bd6217d88c9',1,'Map::MapVertex::v()'],['../structMapVAO_1_1MapVertex.html#a7fd41c17e630e4d6268e045f2cea08b5',1,'MapVAO::MapVertex::v()']]],
   ['vao',['vao',['../classMapVAO.html#a555e725150a4f647cbe4828c9045da42',1,'MapVAO::vao()'],['../classTestVBO.html#a0f595e8e0f3926bc530ef1a38f6021ef',1,'TestVBO::vao()']]],
-  ['vaobase',['vaoBase',['../classVAOPositionColorBase.html#a73482de6ab633d4295643249c4263c87',1,'VAOPositionColorBase::vaoBase()'],['../classVAOPositionTexture.html#a56a7e64798937e7c4322bf7bcf4537a5',1,'VAOPositionTexture::vaoBase()']]],
   ['vbo',['vbo',['../classTestVBO.html#aca6af5577996e9c861c861fe397fe5b4',1,'TestVBO']]],
   ['vertex',['vertex',['../classCountCrossing.html#a35b1f953f77daecfd8a7faaf445f0cf9',1,'CountCrossing']]],
   ['vertexcount',['vertexCount',['../structEstimateSpecResult.html#a5a73c4fcb0d96501ecbceff0d054e805',1,'EstimateSpecResult::vertexCount()'],['../classVAOPositionColorBase.html#aebaaf3f165ae9cc716444f50f0dbb2a1',1,'VAOPositionColorBase::vertexCount()'],['../classVAOPositionTexture.html#a9d189bfc0a801119afe22f38abef42df',1,'VAOPositionTexture::vertexCount()']]],
