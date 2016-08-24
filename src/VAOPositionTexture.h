fbd839516318ef7ecfabc031bc65f5c37b2978a7
verxirtam <d-kosuga@jcom.home.ne.jp>
2016-08-25 00:42:41 +0900

    VAOPositionTextureをインターリーブ配列にした

--- src/VAOPositionTexture.h
+++ src/VAOPositionTexture.h
@@ -31,18 +31,18 @@ class VAOPositionTexture
 {
 private:
 	VAOBase base;
-	VBOStatic position;
-	VBOStatic textureCoord;
+	VBOStatic positionTexture;
 	VBOElementStatic element;
 	GLenum mode;
 	int vertexCount;
 	S& shaderProgram;
 	Texture2D& texture;
+	void initPositionTexture(const std::vector<float>& p, const std::vector<float>& t);
+	
 public:
 	VAOPositionTexture(S& s, Texture2D& t):
 		base(),
-		position(),
-		textureCoord(),
+		positionTexture(),
 		element(),
 		mode(0),
 		vertexCount(0),
@@ -86,6 +86,34 @@ public:
 };
 
 
+template <typename S>
+void VAOPositionTexture<S>::initPositionTexture
+	(
+		const std::vector<float>& p,
+		const std::vector<float>& t
+	)
+{
+	//初期化用のインターリーブ配列
+	std::vector<float> pt;
+	//頂点数の算出
+	unsigned int imax = std::min(p.size() / 3, t.size() / 2);
+	//インターリーブ配列の作成
+	for(unsigned int i = 0; i < imax; i++)
+	{
+		//positionは3つずつ、
+		//textureは2つずつ配置する
+		unsigned int i3 = i * 3;
+		unsigned int i2 = i * 2;
+		
+		pt.push_back(p[i3    ]);
+		pt.push_back(p[i3 + 1]);
+		pt.push_back(p[i3 + 2]);
+		pt.push_back(t[i2    ]);
+		pt.push_back(t[i2 + 1]);
+	}
+	//作成したインターリーブ配列を使用して初期化
+	positionTexture.init(pt);
+}
 
 template <typename S>
 void VAOPositionTexture<S>::init
@@ -99,8 +127,7 @@ void VAOPositionTexture<S>::init
 	base.init();
 	
 	//引数をバッファに格納
-	position.init(p);
-	textureCoord.init(t);
+	initPositionTexture(p, t);
 	element.init(e);
 	//頂点数を格納
 	vertexCount = e.size();
@@ -110,38 +137,33 @@ void VAOPositionTexture<S>::init
 	
 	//自身のVAOをバインド
 	Bind<VAOPositionTexture> b(*this);
-	
-	//positionの設定
+	//positionTextureの設定
 	{
 		//頂点配列をバインド
-		Bind<VBOStatic> bp(position);
-		
+		Bind<VBOStatic> bp(positionTexture);
+		//position部分の設定
 		glVertexAttribPointer
 			(
 				0,		//設定するバーテックスシェーダの引数のインデックスを指定する
 				3,		//1頂点あたりの要素数(ここでは3次元座標なので3)
 				GL_FLOAT,	//要素の型
 				GL_FALSE,	//正規化の要否 位置座標なのでFalse
-				0,		//頂点データ同士の間隔(byte単位) 0なら隙間なく配置されているとみなされる
-				(GLubyte*)NULL	//頂点データの開始アドレスから指定するデータの位置までの間隔
+				5 * sizeof(float),		//頂点データ同士の間隔(byte単位) 0なら隙間なく配置されているとみなされる
+				(GLfloat*)0	//頂点データの開始アドレスから指定するデータの位置までの間隔
 			);
 		glEnableVertexAttribArray(0);
-	}
-	//textureCoordの設定
-	{
-		//テクスチャ座標のの配列をバインド
-		Bind<VBOStatic> bt(textureCoord);
-		
+		//texture部分の設定
 		glVertexAttribPointer
 			(
 				1,		//設定するバーテックスシェーダの引数のインデックスを指定する
-				2,		//1頂点あたりの要素数(ここでは2次元座標なので2)
+				2,		//1頂点あたりの要素数(ここでは3次元座標なので3)
 				GL_FLOAT,	//要素の型
 				GL_FALSE,	//正規化の要否 位置座標なのでFalse
-				0,		//頂点データ同士の間隔(byte単位) 0なら隙間なく配置されているとみなされる
-				(GLubyte*)NULL	//頂点データの開始アドレスから指定するデータの位置までの間隔
+				5 * sizeof(float),		//頂点データ同士の間隔(byte単位) 0なら隙間なく配置されているとみなされる
+				(GLfloat*)0 + 3	//頂点データの開始アドレスから指定するデータの位置までの間隔
 			);
 		glEnableVertexAttribArray(1);
 	}
+	
 }
 
